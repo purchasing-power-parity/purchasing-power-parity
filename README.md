e6c75585a0d6e1b2362853df2d12f1ce4a4698bb
Robin Wieruch <wrobin@gmx.net>
2017-10-29 21:29:11 +0700

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# Simple Node Server with Babel
+
+An easy way to get started with JavaScript on the command line.
+
+## Requirements
+
+* [node & npm](https://nodejs.org/en/)
+* [git](https://www.robinwieruch.de/git-essential-commands/)
+
+## Installation
+
+* `git clone git@github.com:rwieruch/node-babel-server.git`
+* `cd node-babel-server`
+* `npm install`
+* `npm start`
+
+Adjust your Babel presets in *package.json* if you like to add or remove future JavaScript ES features.
+# purchasing-parity-power
---
+++ index.js
@@ -0,0 +1,94 @@
+const axios = require('axios');
+
+const BASE_CURRENCY = 'USD';
+const GEO_API_URL = 'https://freegeoip.net/json/';
+const COUNTRY_META_API = 'https://restcountries.eu/rest/v2/alpha/';
+const EXCHANGE_RATES_API = `https://api.fixer.io/latest?base=${BASE_CURRENCY}`;
+const QUANDL_API = 'https://www.quandl.com/api/v3/';
+
+const getYear = () =>
+  new Date().getFullYear();
+
+const getLastYear = () =>
+  getYear() - 1;
+
+const getQuandlApiUrl = countryCodeIsoAlpha3 =>
+  `${QUANDL_API}datasets/ODA/${countryCodeIsoAlpha3}_PPPEX.json?start_date=${getLastYear()}-01-01&end_date=${getYear()}-01-01`;
+
+const mapGeoToPpp = response => ({
+  countryName: response.data.country_name,
+  countryCodeIsoAlpha2: response.data.country_code,
+});
+
+const getGeo = () =>
+  console.log(`asd`) || axios.get(GEO_API_URL)
+    .then(mapGeoToPpp);
+
+const mapCountryMetaToPpp = pppInformation => response => ({
+  ...pppInformation,
+  currenciesCountry: response.data.currencies,
+  countryCodeIsoAlpha3: response.data.alpha3Code,
+});
+
+const fetchCountryMeta = pppInformation =>
+  axios.get(`${COUNTRY_META_API}${pppInformation.countryCodeIsoAlpha2}`)
+    .then(mapCountryMetaToPpp(pppInformation));
+
+const findExchangeRate = (exchangeRates, currenciesCountry) => {
+  for (let i = 0; i < currenciesCountry.length; i++) {
+    if (exchangeRates[currenciesCountry[i].code]) {
+      return {
+        exchangeRate: exchangeRates[currenciesCountry[i].code],
+        ...currenciesCountry[i],
+      };
+    } else {
+      return {
+        exchangeRate: 1,
+      }
+    }
+  }
+};
+
+const mapExchangeRatesToPpp = pppInformation => response =>
+  ({
+    ...pppInformation,
+    currencyMain: findExchangeRate(response.data.rates, pppInformation.currenciesCountry),
+  });
+
+const fetchExchangedRates = country =>
+  axios.get(EXCHANGE_RATES_API)
+    .then(mapExchangeRatesToPpp(country));
+
+const mapToPpp = pppInformation => response =>
+  ({
+    ...pppInformation,
+    ppp: response.data.dataset.data[0][1],
+  });
+
+const fetchPpp = pppInformation =>
+  axios.get(getQuandlApiUrl(pppInformation.countryCodeIsoAlpha3))
+    .then(mapToPpp(pppInformation));
+
+const computePppConversionFactor = ({ currencyMain, ppp }) =>
+  ppp / currencyMain.exchangeRate;
+
+const getPppConversionFactor = pppInformation => ({
+  ...pppInformation,
+  pppConversionFactor: computePppConversionFactor(pppInformation),
+});
+
+const getPpp = () =>
+  getGeo()
+    .then(fetchCountryMeta)
+    .then(fetchExchangedRates)
+    .then(fetchPpp)
+    .then(getPppConversionFactor)
+    .catch(() => {
+      try {
+        throw new Error('Failed to fetch purchasing parity power.');
+      } catch (e) {
+        throw e;
+      }
+    });
+
+module.export = getPpp;
